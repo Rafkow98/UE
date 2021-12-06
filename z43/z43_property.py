4e43adea377422f58f53f2e35f77575c9433a894
Rafkow98 <93659326+Rafkow98@users.noreply.github.com>
2021-12-06 12:55:50 +0100

    Delete z43 directory

--- z43/z43_property.py
+++
@@ -1,6 +0,0 @@
-class Property:
-    def __init__(self, area, rooms: int, price, address):
-        self.area = area
-        self.rooms = rooms
-        self.price = price
-        self.address = address
