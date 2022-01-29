0fd2bdffbe969c0e68b808a246afb6cfb208a682
Rafkow98 <93659326+Rafkow98@users.noreply.github.com>
2022-01-29 17:48:42 +0100

    Update main.py

--- person_detection/main.py
+++ person_detection/main.py
@@ -4,13 +4,9 @@ import glob
 
 
 def main():
-    cv_img = []
     for img in glob.glob("*.jpg"):
         n = cv2.imread(img)
-        cv_img.append(n)
-
-    for x in cv_img:
-        person_detect(x)
+        person_detect(n)
 
 
 if __name__ == "__main__":
